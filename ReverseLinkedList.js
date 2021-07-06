ee162d0fa37e5beeba3427c2e1608425c01ba830
Rish Agarwal <rish.destiny@gmail.com>
2021-07-06 15:45:50 +0530

    ES6 solution added

--- ReverseLinkedList.js
+++ ReverseLinkedList.js
@@ -16,3 +16,12 @@ var reverseList = function(head) {
     // Prev is the reversed linked list
     return prev;
 };
+
+//ES6 solution:
+var reverseList = function(head) {
+    let [prev, current] = [null, head]
+    while(current) {
+        [current.next, prev, current] = [prev, current, current.next]
+    }
+    return prev
+}
